a5c0b4283157733a7d915c147414b9769e4cd84c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2020-04-08 16:53:09 -0400

    restructured mona client library

--- projects/ng-mona-client/src/public-api.ts
+++ projects/ng-mona-client/src/public-api.ts
@@ -2,9 +2,10 @@
  * Public API Surface of ng-mona-client
  */
 
+export * from './lib/model/auth.model';
 export * from './lib/model/spectrum.model';
 
-export * from './lib/auth.service';
-export * from './lib/spectrum.service';
+export * from './lib/services/auth.service';
+export * from './lib/services/spectrum.service';
 
 export * from './lib/ng-mona-client.module';
