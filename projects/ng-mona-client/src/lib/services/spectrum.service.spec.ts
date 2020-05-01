b6c49440b3a4b1abae9bf41bcda6b93e91dbb3c0
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2020-05-01 10:36:20 -0400

    added complex query test in mona client

--- projects/ng-mona-client/src/lib/services/spectrum.service.spec.ts
+++ projects/ng-mona-client/src/lib/services/spectrum.service.spec.ts
@@ -88,4 +88,29 @@ describe('SpectrumService', () => {
         fail(JSON.stringify(error))
     );
   }));
+
+  it('should execute a complex query with text search', async(() => {
+    const query = `(tags.text=="LC-MS") and (metaData=q='name=="ionization mode" and value=="positive"') and (metaData=q='name=="ms level" and value=="MS2"') and (tags.text=="MassBank") and metaData=q='name=="instrument type" and value=="LC-ESI-QTOF"' and metaData=q='name=="precursor type" and value=="[M+H]+"'`;
+
+    service.list(query, 'massbank', 0, 1).subscribe(
+      (response: Spectrum[]) => {
+        expect(response.length).toEqual(1);
+        expect(response[0].tags.map(x => x.text)).toContain('LC-MS');
+
+        let metaData = response[0].metaData.filter(x => x.name === 'precursor type');
+        expect(metaData.length).toEqual(1);
+        expect(metaData[0].value).toEqual('[M+H]+');
+
+        metaData = response[0].metaData.filter(x => x.name === 'ms level');
+        expect(metaData.length).toEqual(1);
+        expect(metaData[0].value).toEqual('MS2');
+
+        metaData = response[0].metaData.filter(x => x.name === 'ionization mode');
+        expect(metaData.length).toEqual(1);
+        expect(metaData[0].value).toEqual('positive');
+      },
+      (error: HttpErrorResponse) =>
+        fail(JSON.stringify(error))
+    );
+  }));
 });
