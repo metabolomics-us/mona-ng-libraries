676e5878a690cee96fabebc5048c586200557e86
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2020-05-06 08:53:27 -0400

    updated public-api for mona client

--- projects/ng-mona-client/src/public-api.ts
+++ projects/ng-mona-client/src/public-api.ts
@@ -3,9 +3,18 @@
  */
 
 export * from './lib/model/auth.model';
+export * from './lib/model/download.model';
+export * from './lib/model/news.model';
 export * from './lib/model/spectrum.model';
+export * from './lib/model/statistics.model';
 
 export * from './lib/services/auth.service';
+export * from './lib/services/download.service';
+export * from './lib/services/news.service';
 export * from './lib/services/spectrum.service';
+export * from './lib/services/statistics.service';
+
+export { QueryTerm } from './lib/query/query-term.model';
+export * from './lib/query/spectrum-query.service';
 
 export * from './lib/ng-mona-client.module';
