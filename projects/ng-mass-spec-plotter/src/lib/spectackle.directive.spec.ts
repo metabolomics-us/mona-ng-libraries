546edbf9577fda0118ad80d33d1fca4ec9457cf3
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2020-07-27 09:20:02 -0400

    clean up ng-mass-spec-plotter library

--- projects/ng-mass-spec-plotter/src/lib/spectackle.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpectackleDirective } from './spectackle.directive';
-
-describe('SpectackleDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SpectackleDirective();
-    expect(directive).toBeTruthy();
-  });
-});
